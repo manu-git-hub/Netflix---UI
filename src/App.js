939f33bd08329fdac851bfe1460c9aba535322fe
Manu S <ms.manu.856@gmail.com>
2024-03-05 21:56:54 +0530

    JSON Placeholder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
-import NavBar from "./components/NavBar/NavBar";
-import './App.css';
-import Banner from "./components/Banner/Banner";
-import RowPost from "./components/RowPost/RowPost";
+import {useState} from "react";
+import axios from "axios";
+// import NavBar from "./components/NavBar/NavBar";
+// import './App.css';
+// import Banner from "./components/Banner/Banner";
+// import RowPost from "./components/RowPost/RowPost";
 
 function App() {
+  const [state, setState] = useState([]);
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar />
       <Banner />
-      <RowPost />
+      <RowPost /> */}
+
+      {/* API GET */}
+      <h1>Hello world</h1>
+      <button onClick={()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+          console.log(response.data)
+          setState(response.data)
+        })
+      }}>Click Me</button>
+   {state.map((obj, index) => {
+        return (
+          <div>
+            <h2>{index+1}</h2>
+            <h2>{obj.title}</h2>
+            <h4>{obj.body}</h4>
+          </div>
+        )
+      })}
     </div>
   );
 }
